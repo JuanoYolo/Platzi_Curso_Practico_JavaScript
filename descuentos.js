b77681d1d49fd62de30d943ab8e53e596179aaa5
Juano <juanosierra2@gmail.com>
2022-03-09 17:05:51 -0500

    Ejercicio terminado y conectado con HTML

--- descuentos.js
+++ descuentos.js
@@ -13,3 +13,17 @@ function totalDescuento(precioOriginal,descuento){
 //     descuento,
 //     descuentoFinal,
 // });
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    //Llamar a la funcion que escogimos para calcular el descuento
+    const precioDescuento = totalDescuento(priceValue,discountValue); 
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precion con descuento es: $" + precioDescuento;
+}
